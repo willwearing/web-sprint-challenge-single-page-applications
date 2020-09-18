a9c3d03797a11a7f864366cec4fd7b4e8206192b
willwearing <willwearing@gmail.com>
2020-09-18 10:00:33 -0600

    added axios for submit button - will need later

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div>
       <h1>Lambda Eats</h1>
+      <Link to="/OrderForm">
+        <button>Choose Your Pizza</button>
+      </Link>
     </div>
   );
 }
--- src/Components/OrderForm.js
+++ src/Components/OrderForm.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+//submit
+const onSubmit = (event) => {
+  event.preventDefault();
+};
+
 export default function OrderForm() {
   return (
     <div>
       <h2>Design and Order Your Pizza!</h2>
+      <form></form>
     </div>
   );
 }
